a2fdbe32f0e519195e3ab439230f603c95f06dd9
Aleksi Kytölä <marsunpaisti@gmail.com>
2020-01-09 23:55:39 +0200

    added reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const Styles = styled.div`
+		.login {
+			padding: 0.5em;
+    }
+    .login-column {
+      margin: 5em auto 0 auto;
+    }
+	`;
+
+	return (
+		<Styles>
+			<div className="container">
+				<div className="row">
+					<div className="column align-self-center login-column">
+						<div id="login-card" className="card">
+							<div id="login" className="login">
+								<Form>
+									<Form.Group controlId="formBasicEmail">
+										<Form.Label>Email address</Form.Label>
+										<Form.Control type="email" placeholder="Enter email" />
+									</Form.Group>
+
+									<Form.Group controlId="formBasicPassword">
+										<Form.Label>Password</Form.Label>
+										<Form.Control type="password" placeholder="Password" />
+									</Form.Group>
+									<Button variant="primary" type="submit">
+										Submit
+									</Button>
+								</Form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Styles>
+	);
 }
 
 export default App;
